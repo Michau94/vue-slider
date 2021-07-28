5446f13382b6f81a51c1bdfae2afb5e218c14bb5
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-07-28 15:01:03 +0200

    structure + active logic

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 console.log('vue test: ', Vue);
 
+const root = new Vue({
+    el: '#root',
+    data: {
+        currentIndex: 0,
+        images: [
+            './images/image1.jpg',
+            './images/image2.jpg',
+            './images/image3.jpg',
+            './images/image4.jpg',
+        ],
+
+
+    },
+
+    methods: {
+        isActive(index) {
+            return this.currentIndex === index ? 'active' : '';
+        },
+
+
+    }
+
+})
